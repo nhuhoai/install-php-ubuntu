edb7dbc1f67a9793a242a33070a06134944ea6e7
Nhu-Hoai Robert VO <nhuhoai.vo@nhuvo.ch>
2020-12-16 11:45:50 +0100

    Create delphpext.sh

---
+++ delphpext.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apt-cache search "^php*.*-$1$" | awk '{print $1;system("apt remove -y "$1)}'
+
+service apache2 restart
