a9dbcae84bcba6f02467993bb9db8ea2666e8653
Nhu-Hoai Robert VO <nhuhoai.vo@nhuvo.ch>
2020-12-16 11:49:27 +0100

    Improve bash script.

--- delphpext.sh
+++ delphpext.sh
@@ -5,6 +5,6 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-apt-cache search "^php*.*-$1$" | awk '{print $1;system("apt remove -y "$1)}'
+apt-cache search "^php[0-9].[0-9]-$1$" | awk '{print $1;system("apt remove -y "$1)}'
 
 service apache2 restart
